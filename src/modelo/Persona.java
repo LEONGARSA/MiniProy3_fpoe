6b4c54b1e3612da40d9cf46cad6225dd6109847c
LEONGARSA <garcia.leonardo@correounivalle.edu.co>
2023-11-15 17:55:52 -0500

    Clase Persona

---
+++ src/modelo/Persona.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author LEONARDO
+ */
+public class Persona {
+    private String id;
+    private String nombres;
+    private String apellidos;
+    private List direccion;
+    private Date fechaNacimiento;
+    private String telefono;
+    private String ReferenciaTelefonica;    
+    private String RolUniversitario;
+    
+    
+    
+    public Persona(String id, String nombres, String apellidos, List direccion,
+            Date fechaNacimiento, String telefono, String ReferenciaTelefonica,
+            String RolUniversitario) {
+        
+        
+        
+    } 
+}
+
+
