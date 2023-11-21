860cd286f043f72958a19a026698957cc35affa2
LEONGARSA <garcia.leonardo@correounivalle.edu.co>
2023-11-20 20:53:28 -0500

    COMMIT DESPUES DEL CAOS

--- src/co/edu/univalle/contactos/vo/Contacto.java
+++ src/co/edu/univalle/contactos/vo/Contacto.java
@@ -3,11 +3,77 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package co.edu.univalle.contactos.vo;
+import java.util.Date;
+import java.util.List;
 
 /**
  *
  * @author LAURA
  */
 public class Contacto {
+    
+    	private Long identificacion;
+	private String nombres;
+	private String apellidos;
+	private String tipoContacto;
+	private Date fechaNacimiento;
+	private List<String> direcciones;
+	private List<Telefono> telefonos;
+        
+        	public Long getIdentificacion() {
+		return identificacion;
+	}
+
+	public void setIdentificacion(Long identificacion) {
+		this.identificacion = identificacion;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getTipoContacto() {
+		return tipoContacto;
+	}
+
+	public void setTipoContacto(String tipoContacto) {
+		this.tipoContacto = tipoContacto;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public List<String> getDirecciones() {
+		return direcciones;
+	}
+
+	public void setDirecciones(List<String> direcciones) {
+		this.direcciones = direcciones;
+	}
+
+	public List<Telefono> getTelefonos() {
+		return telefonos;
+	}
+
+	public void setTelefonos(List<Telefono> telefonos) {
+		this.telefonos = telefonos;
+	}
     
 }
